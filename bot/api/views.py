4e95d7756e720c4fb8550106a34ff03bf7218f63
ghalebkhaled <ghaleb@foundertherapy.co>
2016-04-19 17:03:23 +0300

    add reply message

--- bot/api/views.py
+++ bot/api/views.py
@@ -1,8 +1,29 @@
+import requests_futures.sessions
+
 import django.conf
 
 from rest_framework.generics import GenericAPIView
 import rest_framework.response
 import rest_framework.status
+
+
+# TODO : we might need to increase number of workers - I think default is 2
+session = requests_futures.sessions.FuturesSession()
+
+
+def send_message_callback(sess, resp):
+    pass
+
+
+def send_message(sender, text):
+    token = django.conf.settings.FB_BOT_ACCESS_TOKEN
+    messageData = {
+        'text': text
+    }
+    session.post('https://graph.facebook.com/v2.6/me/messages?access_token={}'.format(token), json={
+        'recipient': {'id': sender},
+        'message': messageData,
+    }, background_callback=send_message_callback)
 
 
 class WebhookView(GenericAPIView):
@@ -14,14 +35,12 @@ class WebhookView(GenericAPIView):
                                                 data="wrong validation token")
 
     def post(self, request, *args, **kwargs):
-        print request.POST
-        print "----------"
-        print request.data
-        print "----------"
-        print args
-        print "----------"
-        print kwargs
-        print "----------"
-        print request
+        if request.data and request.data['entry']:
+            for entry in request.data['entry']:
+                for msging in entry['messaging']:
+                    for msg in msging:
+                        message = msg['message']
+                        sender_id = msg['sender']['1249712545046598']
+                        send_message(sender_id, "I can only repeat right now:{}".format(message))
 
         return rest_framework.response.Response(status=rest_framework.status.HTTP_200_OK)
